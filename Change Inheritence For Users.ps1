7a870ebd44cb4deb1d20f7b5a3abd0c926c9c043
CozyFriend <96221194+TheShellShaman@users.noreply.github.com>
2025-03-24 11:27:01 -0400

    Add files via upload

---
+++ Change Inheritence For Users.ps1
@@ -0,0 +1,27 @@
+﻿#$users = Get-ADUser -ldapfilter “(objectclass=user)” -searchbase “DC=domain,DC=local”
+$users = get-aduser jondoe #test with a single user first
+
+ForEach($user in $users)
+{
+    $dn= [ADSI](“LDAP://” + $user)
+    $acl= $dn.psbase.objectSecurity
+    if ($acl.get_AreAccessRulesProtected())
+    {
+        $isProtected = $false # $false to enable inheritance
+                              # $true to disable inheritance
+        $preserveInheritance = $true # $true to keep inherited access rules
+                                     # $false to remove inherited access rules.
+                                     # ignored if isProtected=$false
+        $acl.SetAccessRuleProtection($isProtected, $preserveInheritance)
+        $dn.psbase.commitchanges()
+        Write-Host($user.SamAccountName + "|" + `
+                   $user.DistinguishedName + `
+                   "|inheritance set to enabled")
+    }
+    else
+    {
+        write-host($user.SamAccountName + "|" + `
+                   $user.DistinguishedName + `
+                   "|inheritance already enabled")
+    }
+}
